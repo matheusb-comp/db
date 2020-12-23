3b5557d3c79555a8291f9409cc0ddf465d9c6fcf
Matheus <matheusb.comp@gmail.com>
2020-12-23 17:49:22 -0300

    SQLite password: null
    Also adds SQLite folder volume and image version variables

---
+++ config/adminer/login-password-less.php
@@ -0,0 +1,13 @@
+<?php
+
+// Based on:
+// https://github.com/vrana/adminer/blob/master/adminer/sqlite.php
+
+require_once('plugins/login-password-less.php');
+
+/** Set allowed password
+	* @param string result of password_hash
+	*/
+return new AdminerLoginPasswordLess(
+	$password_hash = password_hash('null', PASSWORD_DEFAULT)
+);
